f44eaf7f2acf5899381c2964bd8056424c36f0d1
Rajesh <rajesh@Rajeshs-MacBook-Pro.local>
2023-12-25 09:41:01 +0000

    guess complete but not multiple guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::{io, cmp::Ordering};
+
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world! Gusssing game");
+    println!("Guess the number");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("Please input your guess");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read the line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number");
+
+    println!("Yo guessed: {guess}");
+    println!("Right number: {secret_number}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Greater => println!("Too big"),
+        Ordering::Equal => println!(" You win"),
+    }
 }
